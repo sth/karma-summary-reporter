30a166b954d78c0978c71836775d161ea667e948
Stephan Hohe <sth.dev@tejp.de>
2016-08-27 16:24:17 +0200

    Reset old statistics before new test run

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function strmul(s, n) {
 var SummaryReporter = function(baseReporterDecorator) {
 	baseReporterDecorator(this);
 
-	var specorder = [];
-	var specresults = Object.create(null);
+	var specorder, specresults;
 
 	this.specSuccess = this.specFailure = this.specSkipped = function(browser, result) {
 		var specid = result.suite.join('/') + '/' + result.description;
@@ -26,12 +25,17 @@ var SummaryReporter = function(baseReporterDecorator) {
 	}
 
 
+	// Maximum length of the spec label
 	var speclength = 50;
-	var currentPath = [];
+
+	// Previously printed spec path
+	var currentPath;
 	this.printSpecLabel = function(path) {
 		var indent = "  ";
 		var i;
 		path.forEach(function(s, i) {
+			// We keep the current comon prefix and start to print
+			// the new information on the first difference.
 			if (i < currentPath.length && s != currentPath[i]) {
 				currentPath.length = i;
 			}
@@ -63,15 +67,22 @@ var SummaryReporter = function(baseReporterDecorator) {
 			if (!result.partial)
 				this.writeCommonMsg(' ✓ '.green);
 			else
-				this.writeCommonMsg(' ✓ '.yellow);
+				this.writeCommonMsg('(✓)'.yellow);
 		}
 		else {
 			this.writeCommonMsg(' ✗ '.red);
 		}
 	};
 
+	this.onRunStart = function() {
+		this._browsers = [];
+		currentPath = [];
+		specorder = [];
+		specresults = Object.create(null);
+	}
+
 	this.onRunComplete = function(browsers, results) {
-		this.writeCommonMsg('RESULTS TABLE\n');
+		this.writeCommonMsg('SUMMARY'.bold + '\n');
 		browsers.forEach(function(browser, i) {
 			this.writeCommonMsg(' ' + i + ': ' + this.renderBrowser(browser) + '\n');
 		}, this);
